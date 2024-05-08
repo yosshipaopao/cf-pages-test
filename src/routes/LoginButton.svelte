d139e8bce135c20885f1d70f5cd82d0af99eeb2f
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2024-05-08 22:32:18 +0900

    can not use button inside form without `type='button'`

--- src/routes/LoginButton.svelte
+++ src/routes/LoginButton.svelte
@@ -5,7 +5,7 @@
 
 {#if $page.data.session?.user }
   <div class="flex items-center">
-    <button on:click={()=>signOut()} class="btn btn-outline btn-primary">Logout</button>
+    <button type="button" on:click={()=>signOut()} class="btn btn-outline btn-primary">Logout</button>
     <div class="avatar mx-5">
       <div class="w-10 rounded-full">
         <img src={$page.data.session.user.image || ''} alt={$page.data.session.user.name || ''} width={100} height={100}/>
@@ -14,7 +14,7 @@
     <span class="label-text">{$page.data.session?.user?.name || 'NaN'}</span>
   </div>
 {:else}
-  <button on:click={()=>signIn()} class="login btn btn-wide btn-outline btn-primary">
+  <button type="button" on:click={()=>signIn()} class="login btn btn-wide btn-outline btn-primary">
     Login Discord
   </button>
 {/if}
